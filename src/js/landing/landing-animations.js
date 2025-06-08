4c9bccb44d854cbdb85602cf85baf46b71a5a841
alexhportland <alex.humphries@portland-communications.com>
2025-06-08 12:24:22 +0100

    Delete src directory

--- src/js/landing/landing-animations.js
+++
@@ -1,6 +0,0 @@
-import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.12.5/+esm";
-import ScrollTrigger from "https://cdn.jsdelivr.net/npm/gsap@3.12.5/ScrollTrigger.min.js/+esm";
-
-gsap.registerPlugin(ScrollTrigger);
-
-console.log("Landing animations loaded.");
